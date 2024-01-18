db3b45cc22276b5000ed6a3bbc7c7e51d1b33443
MikeMwita <mwitamike8@gmail.com>
2024-01-18 14:30:01 +0300

    Add Screen layout helper

---
+++ lib/common/helpers/spacing.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// Horizontal Spacing
+Widget horizontalSpaceTiny = const SizedBox(width: 4.0);
+Widget horizontalSpaceSmall = const SizedBox(width: 8.0);
+Widget horizontalSpaceRegular = const SizedBox(width: 16.0);
+Widget horizontalSpaceMedium = const SizedBox(width: 24.0);
+Widget horizontalSpaceLarge = const SizedBox(width: 48.0);
+
+// Vertical Spacing
+Widget verticalSpaceTiny = const SizedBox(height: 4.0);
+Widget verticalSpaceSmall = const SizedBox(height: 8.0);
+Widget verticalSpaceRegular = const SizedBox(height: 16.0);
+Widget verticalSpaceMedium = const SizedBox(height: 24.0);
+Widget verticalSpaceLarge = const SizedBox(height: 48.0);
+Widget verticalSpaceLargeExtra = const SizedBox(height: 70.0);
