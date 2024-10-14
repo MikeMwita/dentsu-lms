0ebf20923cb530e75191b7fbacdcde4fe5390333
MikeMwita <mwitamike8@gmail.com>
2024-10-14 23:11:43 +0300

    chore: Update models

--- lib/models/leads.dart
+++ lib/models/leads.dart
@@ -1,6 +1,10 @@
 class Lead {
+  /// Creates the employee class with required details.
   Lead(this.id, this.name);
 
+  /// Id of an employee.
   final String id;
+
+  /// Name of an employee.
   final String name;
 }
